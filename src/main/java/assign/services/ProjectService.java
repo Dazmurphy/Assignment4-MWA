609438d672494def1df2819c8d8b5a964ddcd6b8
dazmurphy <daz.murphy.94@gmail.com>
2018-10-30 19:15:47 +0000

    removed whitespaces

--- src/main/java/assign/services/ProjectService.java
+++ src/main/java/assign/services/ProjectService.java
@@ -5,13 +5,13 @@ import assign.domain.Project;
 public interface ProjectService {
 
 	public Project addProject(Project p) throws Exception;
-	
+
 	public Project getProject(int projectId) throws Exception;
-	
+
 	public Project updateProject(Project p) throws Exception;
-	
+
 	public Project deleteProject(int projectId) throws Exception;
-	
+
 	public boolean checkProjectId(int projectId) throws Exception;
 
 }
--- src/main/java/assign/services/ProjectServiceImpl.java
+++ src/main/java/assign/services/ProjectServiceImpl.java
@@ -24,46 +24,44 @@ public class ProjectServiceImpl implements ProjectService {
 		this.dbURL = dbUrl;
 		this.dbUsername = username;
 		this.dbPassword = password;
-		
+
 		ds = setupDataSource();
 	}
-	
+
 	public DataSource setupDataSource() {
-        BasicDataSource ds = new BasicDataSource();
-        ds.setUsername(this.dbUsername);
-        ds.setPassword(this.dbPassword);
-        ds.setUrl(this.dbURL);
-        ds.setDriverClassName("com.mysql.jdbc.Driver");
-        return ds;
-    }
-	
+		BasicDataSource ds = new BasicDataSource();
+		ds.setUsername(this.dbUsername);
+		ds.setPassword(this.dbPassword);
+		ds.setUrl(this.dbURL);
+		ds.setDriverClassName("com.mysql.jdbc.Driver");
+		return ds;
+	}
+
 	public Project addProject(Project p) throws Exception {
 		Connection conn = ds.getConnection();
-		
+
 		String insert = "INSERT INTO projects(name, description) VALUES(?, ?)";
-		PreparedStatement stmt = conn.prepareStatement(insert,
-                Statement.RETURN_GENERATED_KEYS);
-		
+		PreparedStatement stmt = conn.prepareStatement(insert, Statement.RETURN_GENERATED_KEYS);
+
 		stmt.setString(1, p.getName());
 		stmt.setString(2, p.getDescription());
-		
+
 		int affectedRows = stmt.executeUpdate();
 
-        if (affectedRows == 0) {
-            throw new SQLException("Creating project failed, no rows affected.");
-        }
-        
-        ResultSet generatedKeys = stmt.getGeneratedKeys();
-        if (generatedKeys.next()) {
-        	p.setProjectId(generatedKeys.getInt(1));
-        }
-        else {
-            throw new SQLException("Creating project failed, no ID obtained.");
-        }
-        
-        // Close the connection
-        conn.close();
-        
+		if (affectedRows == 0) {
+			throw new SQLException("Creating project failed, no rows affected.");
+		}
+
+		ResultSet generatedKeys = stmt.getGeneratedKeys();
+		if (generatedKeys.next()) {
+			p.setProjectId(generatedKeys.getInt(1));
+		} else {
+			throw new SQLException("Creating project failed, no ID obtained.");
+		}
+
+		// Close the connection
+		conn.close();
+
 		return p;
 	}
 
@@ -72,11 +70,11 @@ public class ProjectServiceImpl implements ProjectService {
 		Connection conn = ds.getConnection();
 		PreparedStatement s = conn.prepareStatement(query);
 		ResultSet r = s.executeQuery();
-		
+
 		if (!r.next()) {
 			return null;
 		}
-		
+
 		Project p = new Project();
 		p.setDescription(r.getString("description"));
 		p.setName(r.getString("name"));
@@ -84,48 +82,44 @@ public class ProjectServiceImpl implements ProjectService {
 		return p;
 	}
 
-
 	public Project updateProject(Project p) throws Exception {
 		Connection conn = ds.getConnection();
 		String update = "UPDATE projects SET name = ?, description = ? WHERE projectId = ?";
-		
+
 		PreparedStatement s = conn.prepareStatement(update, Statement.RETURN_GENERATED_KEYS);
-		
+
 		s.setString(1, p.getName());
 		s.setString(2, p.getDescription());
 		s.setInt(3, p.getProjectId());
-		
-		int affectedRows = s.executeUpdate();
-		
 
-        if (affectedRows == 0) {
-            throw new SQLException("Updating project failed, no rows affected.");
-        }
-		
-        conn.close();
-		
+		int affectedRows = s.executeUpdate();
+
+		if (affectedRows == 0) {
+			throw new SQLException("Updating project failed, no rows affected.");
+		}
+
+		conn.close();
+
 		return p;
 	}
 
-
 	public Project deleteProject(int projectId) throws Exception {
-		
+
 		Connection conn = ds.getConnection();
 		String delete = "DELETE from projects WHERE projectId = ?";
-		
-		PreparedStatement s = conn.prepareStatement(delete, Statement.RETURN_GENERATED_KEYS);
-		
-		s.setInt(1, projectId);
-		
-		int affectedRows = s.executeUpdate();
-		
 
-        if (affectedRows == 0) {
-            throw new SQLException("Deleting project failed, no rows affected.");
-        }
-		
-        conn.close();
-		
+		PreparedStatement s = conn.prepareStatement(delete, Statement.RETURN_GENERATED_KEYS);
+
+		s.setInt(1, projectId);
+
+		int affectedRows = s.executeUpdate();
+
+		if (affectedRows == 0) {
+			throw new SQLException("Deleting project failed, no rows affected.");
+		}
+
+		conn.close();
+
 		return null;
 	}
 
@@ -133,22 +127,22 @@ public class ProjectServiceImpl implements ProjectService {
 	public boolean checkProjectId(int projectId) throws Exception {
 		Connection conn = ds.getConnection();
 		String checkId = "SELECT * from projects WHERE projectId = ?";
-		
+
 		PreparedStatement s = conn.prepareStatement(checkId, Statement.RETURN_GENERATED_KEYS);
-		
+
 		s.setInt(1, projectId);
-		
+
 		ResultSet affectedRows = s.executeQuery();
-		
+
 		affectedRows.last();
 		int count = affectedRows.getRow();
-		
-        if (count == 0) {
-            return false;
-        }
-		
-        conn.close();
-		
+
+		if (count == 0) {
+			return false;
+		}
+
+		conn.close();
+
 		return true;
 	}
 }
